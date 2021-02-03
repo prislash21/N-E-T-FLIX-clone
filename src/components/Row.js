de903d175820c4bfb30d31923be43cbec02b43da
prislash21 <prislash21@gmail.com>
2021-02-03 21:05:55 +0600

    R

---
+++ src/components/Row.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
+import requests from '../requests';
+import './Row.css';
+
+const baseImgUrl = 'https://image.tmdb.org/t/p/original';
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.table(movies);
+
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='row__posters'>
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+            src={`${baseImgUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
